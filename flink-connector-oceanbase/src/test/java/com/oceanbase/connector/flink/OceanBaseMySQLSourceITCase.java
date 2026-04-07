679eadc43841fd0d761dab7a5547d392b45d13d1
yuanoOo <zhaomingyuan.zmy@digital-engine.com>
2026-04-07 17:12:09 +0800

    test: add all-types integration test for parallel snapshot source
    Add OceanBaseMySQLSourceITCase covering 15 data types (BOOLEAN, TINYINT,
    SMALLINT, INT, BIGINT, FLOAT, DOUBLE, DECIMAL, CHAR, VARCHAR, DATE,
    TIME, TIMESTAMP, BINARY, VARBINARY) through the full Source read -> Sink
    write -> JDBC query pipeline with TestContainers.

---
+++ flink-connector-oceanbase/src/test/java/com/oceanbase/connector/flink/OceanBaseMySQLSourceITCase.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2024 OceanBase.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.oceanbase.connector.flink;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+
+import java.util.Arrays;
+import java.util.List;
+
+/** Integration tests for OceanBase parallel snapshot read with all supported data types. */
+public class OceanBaseMySQLSourceITCase extends OceanBaseMySQLTestBase {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OceanBaseMySQLSourceITCase.class);
+
+    @BeforeAll
+    public static void setup() {
+        CONTAINER.withLogConsumer(new Slf4jLogConsumer(LOG)).start();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        CONTAINER.stop();
+    }
+
+    @Test
+    public void testSourceAllTypes() throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        StreamTableEnvironment tEnv =
+                StreamTableEnvironment.create(
+                        env, EnvironmentSettings.newInstance().inStreamingMode().build());
+
+        initialize("sql/mysql/all_types_source.sql");
+
+        // Create source table using oceanbase connector with parallel read
+        tEnv.executeSql(
+                "CREATE TEMPORARY TABLE source_table ("
+                        + " `id` INT NOT NULL,"
+                        + " col_boolean BOOLEAN,"
+                        + " col_tinyint TINYINT,"
+                        + " col_smallint SMALLINT,"
+                        + " col_int INT,"
+                        + " col_bigint BIGINT,"
+                        + " col_float FLOAT,"
+                        + " col_double DOUBLE,"
+                        + " col_decimal DECIMAL(20, 10),"
+                        + " col_char CHAR(10),"
+                        + " col_varchar VARCHAR(255),"
+                        + " col_date DATE,"
+                        + " col_time TIME(0),"
+                        + " col_timestamp TIMESTAMP(0),"
+                        + " col_binary BINARY(16),"
+                        + " col_varbinary VARBINARY(255),"
+                        + " PRIMARY KEY (`id`) NOT ENFORCED"
+                        + ") with ("
+                        + "  'connector'='oceanbase',"
+                        + "  'table-name'='all_types_source',"
+                        + "  'compatible-mode'='MySQL',"
+                        + "  'split-size'='2',"
+                        + getOptionsString()
+                        + ");");
+
+        // Create sink table
+        tEnv.executeSql(
+                "CREATE TEMPORARY TABLE sink_table ("
+                        + " `id` INT NOT NULL,"
+                        + " col_boolean BOOLEAN,"
+                        + " col_tinyint TINYINT,"
+                        + " col_smallint SMALLINT,"
+                        + " col_int INT,"
+                        + " col_bigint BIGINT,"
+                        + " col_float FLOAT,"
+                        + " col_double DOUBLE,"
+                        + " col_decimal DECIMAL(20, 10),"
+                        + " col_char CHAR(10),"
+                        + " col_varchar VARCHAR(255),"
+                        + " col_date DATE,"
+                        + " col_time TIME(0),"
+                        + " col_timestamp TIMESTAMP(0),"
+                        + " col_binary BINARY(16),"
+                        + " col_varbinary VARBINARY(255),"
+                        + " PRIMARY KEY (`id`) NOT ENFORCED"
+                        + ") with ("
+                        + "  'connector'='oceanbase',"
+                        + "  'table-name'='all_types_sink',"
+                        + getOptionsString()
+                        + ");");
+
+        tEnv.executeSql("INSERT INTO sink_table SELECT * FROM source_table").await();
+
+        waitingAndAssertTableCount("all_types_sink", 3);
+
+        // Query with HEX() for binary columns
+        List<String> fields =
+                Arrays.asList(
+                        "id",
+                        "col_boolean",
+                        "col_tinyint",
+                        "col_smallint",
+                        "col_int",
+                        "col_bigint",
+                        "col_float",
+                        "col_double",
+                        "col_decimal",
+                        "col_char",
+                        "col_varchar",
+                        "col_date",
+                        "col_time",
+                        "col_timestamp",
+                        "HEX(col_binary)",
+                        "HEX(col_varbinary)");
+
+        List<String> expected =
+                Arrays.asList(
+                        "1,1,127,32767,2147483647,9223372036854775807,3.14,3.14159265358979,12345.6789012345,hello     ,world,2025-01-15,12:34:56,2025-01-15 10:30:00.0,48656C6C6F000000000000000000000000,466C696E6B",
+                        "2,0,-128,-32768,-2147483648,-9223372036854775808,-1.5,-2.718281828,99999.9999999999,test      ,OceanBase,2000-06-30,23:59:59,2000-12-31 23:59:59.0,0102030405060708090A0B0C0D0E0F10,DEADBEEF",
+                        "3,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null");
+
+        List<String> actual = queryTable("all_types_sink", fields);
+
+        assertEqualsInAnyOrder(expected, actual);
+
+        dropTables("all_types_source", "all_types_sink");
+    }
+}
